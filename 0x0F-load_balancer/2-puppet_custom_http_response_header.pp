d2c69eb0632ed21833526b7aa15d9e44e91c1afa
izimartin <izimartinz@gmail.com>
2022-11-01 00:43:27 +0100

    commit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
